a72171beaf9aa021c814fc620c47ad8db3d0b0b1
Arda Kilic <adk_05@outlook.com>
2024-11-15 18:12:10 +0100

    removed comments

--- scripts/script.js
+++ scripts/script.js
@@ -7,71 +7,64 @@ const exitGame = document.getElementById("exitGame");
 let isJumping = false;
 let gameOver = false;
 
-//Jumping function
 function jump() {
-    if (isJumping) return; //prevent double jump for now
+  if (isJumping) return;
 
-    isJumping = true;
-    player.classList.add("jump");
+  isJumping = true;
+  player.classList.add("jump");
 
-    //Once animation is over remove the jump class
-    player.addEventListener("animationend", () => {
-        player.classList.remove("jump");
-        isJumping = false;
-    }, { once: true });
+  player.addEventListener(
+    "animationend",
+    () => {
+      player.classList.remove("jump");
+      isJumping = false;
+    },
+    { once: true }
+  );
 }
 
-// Event listener for the spacebar
 window.addEventListener("keydown", (e) => {
-    if (e.code === "Space") {
-        jump();
-    }else{
-        return null;
-    }
+  if (e.code === "Space") {
+    jump();
+  } else {
+    return null;
+  }
 });
 
 function checkCollision() {
-    //Bounding rectangles of the player and obstacle
-    const playerRect = player.getBoundingClientRect();
-    const obstacleRect = obstacle.getBoundingClientRect();
+  const playerRect = player.getBoundingClientRect();
+  const obstacleRect = obstacle.getBoundingClientRect();
 
-    //Check if the rectangles collide
-    if (
-        playerRect.left <= obstacleRect.right &&
-        playerRect.right >= obstacleRect.left &&
-        playerRect.top <= obstacleRect.bottom &&
-        playerRect.bottom >= obstacleRect.top
-    ) {
-        gameOver = true;
-        endGame();
-    }
+  if (
+    playerRect.left <= obstacleRect.right &&
+    playerRect.right >= obstacleRect.left &&
+    playerRect.top <= obstacleRect.bottom &&
+    playerRect.bottom >= obstacleRect.top
+  ) {
+    gameOver = true;
+    endGame();
+  }
 }
 
 function endGame() {
-    obstacle.style.animation = 'none';
-    //alert("Game Over! You collided with the obstacle.");
-    gameOverModal.style.display = 'flex';
+  obstacle.style.animation = "none";
+  gameOverModal.style.display = "flex";
 }
 
-
 playAgain.addEventListener("click", () => {
-    gameOverModal.style.display = 'none'; 
-    gameLoop();
+  gameOverModal.style.display = "none";
+  gameLoop();
 });
-
 
 exitGame.addEventListener("click", () => {
-    gameOverModal.style.display = 'none'; 
-  
+  gameOverModal.style.display = "none";
 });
 
-
-//Continuously check for collision
 function gameLoop() {
-    if (!gameOver) {
-        checkCollision();
-        requestAnimationFrame(gameLoop);
-    }
+  if (!gameOver) {
+    checkCollision();
+    requestAnimationFrame(gameLoop);
+  }
 }
 
 gameLoop();
